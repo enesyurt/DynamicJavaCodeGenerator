4e634a0752da527645da8a0a88276756c860be1a
Enes Yurt <enesyurt78@gmail.com>
2024-06-24 14:52:01 +0300

    Added TestInfogramDefinitionModel.java GeoPositionType.java, CommonTypes.java

---
+++ generated/CommonTypes.java
@@ -0,0 +1,17 @@
+package test.model.common.datatype;
+public class CommonTypes {
+
+    public enum TestInfogramEnum1Type {
+
+        NO_STATEMENT,
+        NOT_AVAILABLE,
+        PENDING,
+        AVAILABLE;
+    }
+    public enum TestInfogramEnum2Type {
+
+        NO_STATEMENT,
+        PORT,
+        STBD;
+    }
+}
---
+++ generated/TestInfogramDefinitionModel.java
@@ -0,0 +1,64 @@
+package test.model.common;
+import java.util.ArrayList;
+
+public class TestInfogramDefinitionModel {
+
+    private  Id;
+    private  ItemId;
+    private String ItemName;
+    private String Enum1;
+    private String Enum2;
+    private  Percentage;
+    private String PointList;
+    private  PointListSize;
+
+     // Getters and Setters 
+    public  getId() {
+        return Id;
+    }
+    public void setId( Id) {
+        this.Id = Id;
+    }
+    public  getItemId() {
+        return ItemId;
+    }
+    public void setItemId( ItemId) {
+        this.ItemId = ItemId;
+    }
+    public String getItemName() {
+        return ItemName;
+    }
+    public void setItemName(String ItemName) {
+        this.ItemName = ItemName;
+    }
+    public String getEnum1() {
+        return Enum1;
+    }
+    public void setEnum1(String Enum1) {
+        this.Enum1 = Enum1;
+    }
+    public String getEnum2() {
+        return Enum2;
+    }
+    public void setEnum2(String Enum2) {
+        this.Enum2 = Enum2;
+    }
+    public  getPercentage() {
+        return Percentage;
+    }
+    public void setPercentage( Percentage) {
+        this.Percentage = Percentage;
+    }
+    public String getPointList() {
+        return PointList;
+    }
+    public void setPointList(String PointList) {
+        this.PointList = PointList;
+    }
+    public  getPointListSize() {
+        return PointListSize;
+    }
+    public void setPointListSize( PointListSize) {
+        this.PointListSize = PointListSize;
+    }
+}
