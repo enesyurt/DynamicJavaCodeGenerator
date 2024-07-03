3aada31301fa527ff343e3de48412d8d8975606a
Enes Yurt <enesyurt78@gmail.com>
2024-07-03 19:31:22 +0300

    Added new generated file due to change of TestInfogramDefinition3.xml

---
+++ generated/GeoPositionType4Model.java
@@ -0,0 +1,30 @@
+package test.model.common;
+
+import test.LogHelper;
+
+public class GeoPositionType4Model {
+
+    private double latitude;
+    private double longitude;
+    private double altitudeDepth;
+
+    // Getters and Setters
+    public double getLatitude() {
+        return Latitude;
+    }
+    public void setLatitude(double Latitude) {
+        this.Latitude = Latitude;
+    }
+    public double getLongitude() {
+        return Longitude;
+    }
+    public void setLongitude(double Longitude) {
+        this.Longitude = Longitude;
+    }
+    public double getAltitudeDepth() {
+        return AltitudeDepth;
+    }
+    public void setAltitudeDepth(double AltitudeDepth) {
+        this.AltitudeDepth = AltitudeDepth;
+    }
+}
