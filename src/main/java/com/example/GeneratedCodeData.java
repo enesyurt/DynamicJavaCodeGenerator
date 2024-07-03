85805cff11109bea3491465446629d18b9eb2dd6
Enes Yurt <enesyurt78@gmail.com>
2024-07-03 19:31:50 +0300

    Added GeneratedCodeData.java

---
+++ src/main/java/com/example/GeneratedCodeData.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class GeneratedCodeData {
+
+    public enum GeneratedCodeType {
+        EnumType, 
+        RecordType,
+        MainModelType;
+    }
+    
+    public GeneratedCodeType codeType;
+    public String fileName;
+    public StringBuilder fileContent;
+}
